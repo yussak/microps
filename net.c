a7d533580217aa1eb192b409bf9792a7ea2e701b
yussak <yusuke.sakuraba0213@gmail.com>
2026-05-03 01:26:58 +0900

    completion of step1

--- net.c
+++ net.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
 #include <stddef.h>
 #include <stdint.h>
+#include <stdio.h>
 
 #include "platform.h"
 
-#include "util.h"
 #include "net.h"
+#include "util.h"
 
 /*
  * NOTE: if you want to add/delete the entries after net_run(),
@@ -13,66 +13,102 @@
  */
 static struct net_device *devices;
 
-struct net_device *
-net_device_alloc(void)
-{
+struct net_device *net_device_alloc(void) {
+  struct net_device *dev;
+
+  dev = memory_alloc(sizeof(*dev));
+  if (!dev) {
+    errorf("memory_alloc() failure");
+    return NULL;
+  }
+  return dev;
 }
 
 /*
  * NOTE: must not be call after net_run()
  */
-int
-net_device_register(struct net_device *dev)
-{
-}
+int net_device_register(struct net_device *dev) {
+  static unsigned int index = 0;
 
-static int
-net_device_open(struct net_device *dev)
-{
-}
-
-static int
-net_device_close(struct net_device *dev)
-{
-}
-
-int
-net_device_output(struct net_device *dev, uint16_t type, const uint8_t *data, size_t len, const void *dst)
-{
-}
-
-int
-net_init(void)
-{
-    infof("initialize...");
-    if (platform_init() == -1) {
-        errorf("platform_init() failure");
-        return -1;
-    }
-    infof("success");
+  dev->index = index++;
+  snprintf(dev->name, sizeof(dev->name), "net%d", dev->index);
+  dev->next = devices;
+  devices = dev;
+  infof("success,dev=%s,type=0x%04x", dev->name, dev->type);
   return 0;
 }
 
-int
-net_run(void)
-{
-    infof("startup...");
-    if (platform_run() == -1) {
-        errorf("platform_run() failure");
-        return -1;
-    }
-    infof("success");
-    return 0;
+static int net_device_open(struct net_device *dev) {
+  infof("dev=%s", dev->name);
+  if (NET_DEVICE_IS_UP(dev)) {
+    errorf("already opened, dev=%s", dev->name);
+    return -1;
+  }
+  dev->flags |= NET_DEVICE_FLAG_UP;
+  return 0;
 }
 
-int
-net_shutdown(void)
-{
-    infof("shutting down...");
-    if (platform_shutdown() == -1) {
-        warnf("platform_shutdown() failure");
-    } else {
-        infof("success");
-    }
-    return 0;
+static int net_device_close(struct net_device *dev) {
+  if (!NET_DEVICE_IS_UP(dev)) {
+    errorf("not opened, dev=%s", dev->name);
+    return -1;
+  }
+
+  dev->flags &= ~NET_DEVICE_FLAG_UP;
+  return 0;
+}
+
+int net_device_output(struct net_device *dev, uint16_t type,
+                      const uint8_t *data, size_t len, const void *dst) {
+  debugf("dev=%s, type=0x%04x, len=%zu", dev->name, type, len);
+  debugdump(data, len);
+
+  if (!NET_DEVICE_IS_UP(dev)) {
+    errorf("not opened, dev=%s", dev->name);
+    return -1;
+  }
+  if (len > dev->mtu) {
+    errorf("too long, dev=%s, mtu=%u, len=%zu", dev->name, dev->mtu, len);
+    return -1;
+  }
+  return 0;
+}
+
+int net_init(void) {
+  infof("initialize...");
+  if (platform_init() == -1) {
+    errorf("platform_init() failure");
+    return -1;
+  }
+  infof("success");
+  return 0;
+}
+
+int net_run(void) {
+  struct net_device *dev;
+
+  infof("startup...");
+  if (platform_run() == -1) {
+    errorf("platform_run() failure");
+    return -1;
+  }
+  for (dev = devices; dev; dev = dev->next) {
+    net_device_open(dev);
+  }
+  infof("success");
+  return 0;
+}
+
+int net_shutdown(void) {
+  struct net_device *dev;
+
+  infof("shutting down...");
+  if (platform_shutdown() == -1) {
+    warnf("platform_shutdown() failure");
+  }
+  for (dev = devices; dev; dev = dev->next) {
+    net_device_close(dev);
+  }
+  infof("success");
+  return 0;
 }
--- test/test.c
+++ test/test.c
@@ -1,80 +1,102 @@
+#include <errno.h>
+#include <signal.h>
 #include <stdlib.h>
 #include <string.h>
-#include <signal.h>
-#include <errno.h>
 #include <unistd.h>
 
-#include "util.h"
 #include "net.h"
+#include "util.h"
 
 #include "test.h"
 
 static volatile sig_atomic_t terminate;
 
-static void
-on_signal(int signum)
-{
-    (void)signum;
-    terminate = 1;
+static struct net_device *dev;
+
+struct net_device *dummy_init(void) {
+  struct net_device *dev;
+
+  dev = net_device_alloc();
+  if (!dev) {
+    errorf("net_device_alloc() failure");
+    return NULL;
+  }
+  dev->type = NET_DEVICE_TYPE_DUMMY;
+  dev->mtu = 128;
+  dev->hlen = 0;
+  dev->alen = 0;
+  if (net_device_register(dev) == -1) {
+    errorf("net_device_register() failure");
+    return NULL;
+  }
+  infof("success, dev=%s", dev->name);
+  return dev;
 }
 
-static int
-setup(void)
-{
-    struct sigaction sa = {0};
-
-    sa.sa_handler = on_signal;
-    if (sigaction(SIGINT, &sa, NULL) == -1) {
-        errorf("sigaction() %s", strerror(errno));
-        return -1;
-    }
-    infof("setup protocol stack...");
-    if (net_init() == -1) {
-        errorf("net_init() failure");
-        return -1;
-    }
-    if (net_run() == -1) {
-        errorf("net_run() failure");
-        return -1;
-    }
-    return 0;
+static void on_signal(int signum) {
+  (void)signum;
+  terminate = 1;
 }
 
-static int
-cleanup(void)
-{
-    infof("cleanup protocol stack...");
-    if (net_shutdown() == -1) {
-        errorf("net_shutdown() failure");
-        return -1;
-    }
-    return 0;
+static int setup(void) {
+  struct sigaction sa = {0};
+
+  sa.sa_handler = on_signal;
+  if (sigaction(SIGINT, &sa, NULL) == -1) {
+    errorf("sigaction() %s", strerror(errno));
+    return -1;
+  }
+  infof("setup protocol stack...");
+  if (net_init() == -1) {
+    errorf("net_init() failure");
+    return -1;
+  }
+  dev = dummy_init();
+  if (!dev) {
+    errorf("dummy_init() failure");
+    return -1;
+  }
+  if (net_run() == -1) {
+    errorf("net_run() failure");
+    return -1;
+  }
+  return 0;
 }
 
-static int
-app_main(void)
-{
-    debugf("Press Ctrl+C to terminate");
-    while (!terminate) {
-        sleep(1);
-    }
-    debugf("terminate");
-    return 0;
+static int cleanup(void) {
+  infof("cleanup protocol stack...");
+  if (net_shutdown() == -1) {
+    errorf("net_shutdown() failure");
+    return -1;
+  }
+  return 0;
 }
 
-int
-main(void)
-{
-    int ret;
+static int app_main(void) {
+  debugf("Press Ctrl+C to terminate");
+  while (!terminate) {
+    if (net_device_output(dev, 0x0800, test_data, sizeof(test_data), NULL) ==
+        -1) {
+      errorf("net_device_output() failure");
+      break;
+    }
+    sleep(1);
+  }
+  debugf("terminate");
+  return 0;
+}
 
-    if (setup() == -1) {
-        errorf("setup() failure");
-        return -1;
-    }
-    ret = app_main();
-    if (cleanup() == -1) {
-        errorf("cleanup() failure");
-        return -1;
-    }
-    return ret;
+int main(void) {
+  int ret;
+
+  if (setup() == -1) {
+    errorf("setup() failure");
+    return -1;
+  }
+  ret = app_main();
+  if (cleanup() == -1) {
+    errorf("cleanup() failure");
+    return -1;
+  }
+  return ret;
 }
